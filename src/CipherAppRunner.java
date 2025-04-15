8135203686c62534eb85833763a6df3e8be57de4
Pasha Procenko <pashaprocenk@icloud.com>
2025-04-15 14:00:10 +0300

    New Class Constants and Fixed CipherAppRunner

--- src/CipherAppRunner.java
+++ src/CipherAppRunner.java
@@ -2,13 +2,14 @@ import java.io.IOException;
 
 public class CipherAppRunner {
     public void start(String[] args) {
-        if (args.length < 5) {
-            System.out.println("Please use file format: java -jar myApp.jar <ENCRYPT|DECRYPT|BRUTE_FORCE> <filePath> [key]");
+        if (args.length < CommandConstants.MIN_ARGS) {
+            System.out.println("Please use File format: java -jar myApp.jar <ENCRYPT|DECRYPT|BRUTE_FORCE> <filePath> [key]");
             return;
         }
-        String command = args[0].toUpperCase();
-        String filePath = args[1];
-        int key = args.length > 5 ? Integer.parseInt(args[2]) : 0;
+
+        String command = args[CommandConstants.COMMAND_INDEX].toUpperCase();
+        String filePath = args[CommandConstants.FILE_INDEX];
+        int key = args.length > CommandConstants.KEY_INDEX ? Integer.parseInt(args[CommandConstants.KEY_INDEX]) : 0;
 
         dispatcher(command, filePath, key);
     }
@@ -20,17 +21,17 @@ public class CipherAppRunner {
             String outputFilePath;
 
             switch (command) {
-                case "ENCRYPT":
+                case CommandConstants.ENCRYPT:
                     result = CaesarCipher.encrypt(content, key);
-                    outputFilePath = filePath.replace(".txt", "[ENCRYPTED].txt");
+                    outputFilePath = filePath.replace(".txt", CommandConstants.ENCRYPTED_SUFFIX);
                     break;
-                case "DECRYPT":
+                case CommandConstants.DECRYPT:
                     result = CaesarCipher.decrypt(content, key);
-                    outputFilePath = filePath.replace("[ENCRYPTED].txt", "[DECRYPTED].txt");
+                    outputFilePath = filePath.replace(CommandConstants.ENCRYPTED_SUFFIX, CommandConstants.DECRYPTED_SUFFIX);
                     break;
-                case "BRUTE_FORCE":
+                case CommandConstants.BRUTE_FORCE:
                     result = BruteForceKey.bruteForce(content);
-                    outputFilePath = filePath.replace("[ENCRYPTED].txt", "[BRUTEFORCED].txt");
+                    outputFilePath = filePath.replace(CommandConstants.ENCRYPTED_SUFFIX, CommandConstants.BRUTE_FORCED_SUFFIX);
                     break;
                 default:
                     System.out.println("Invalid command. Use ENCRYPT, DECRYPT, or BRUTE_FORCE.");
